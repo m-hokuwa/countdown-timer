de0e8c820f40f4e9bbb95f25c1a948b650447918
ほくわ <m.hokuwa.dev@gmail.com>
2018-06-16 18:21:02 +0900

    Revert "Revert "初版アップロード""
    This reverts commit dff4428dafef62ecfd6bc58908ab6cb203516206.

---
+++ main.js
@@ -0,0 +1,44 @@
+"use strct";
+
+// Electronのモジュール
+const electron = require("electron");
+// const easytimer = require("easytimer/dist/easytimer.min.js");
+// const jquery = require("jquery/dist/jquery.min.js");
+
+// アプリケーションをコントロールするモジュール
+const app = electron.app;
+
+// ウィンドウを作成するモジュール
+const BrowserWindow = electron.BrowserWindow;
+
+// メインウィンドウはGCされないようにグローバル宣言
+let mainWindow = null;
+
+// 全てのウィンドウが閉じたら終了
+app.on("window-all-closed", () => {
+  if (process.platform != "darwin") {
+    app.quit();
+  }
+});
+
+
+// Electronの初期化完了後に実行
+app.on("ready", () => {
+  //ウィンドウサイズを1280*720（フレームサイズを含まない）に設定する
+  mainWindow = new BrowserWindow({
+    width: 1280, 
+    height: 720, 
+    maxWidth: 1280,
+    maxHeight: 720,
+    minWidth: 430,
+    minHeight: 570,
+    useContentSize: true
+  });
+  //使用するhtmlファイルを指定する
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
